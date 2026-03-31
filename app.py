698235440bec97f3367fe0e6ce27ed07b4af8664
Carl Seaholm <cdseaholm@gmail.com>
2026-03-31 19:31:06 +0000

    Final Steps

--- app.py
+++ app.py
@@ -28,3 +28,53 @@ def creating():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://postgres3308_user:4mW94qBdAWY5TnzZHKaY3cdA0cV8uEkD@dpg-d761n7ea2pns73fmf080-a/postgres3308")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://postgres3308_user:4mW94qBdAWY5TnzZHKaY3cdA0cV8uEkD@dpg-d761n7ea2pns73fmf080-a/postgres3308")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+    ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string=""
+    response_string+="<table>"
+    for player in records:
+        response_string+="<tr>"
+        for info in player:
+            response_string+="<td>{}</td>".format(info)
+        response_string+="</tr>"
+    response_string+="</table>"
+    return response_string
+
+@app.route('/db_drop')
+def dropping():
+    conn = psycopg2.connect("postgresql://postgres3308_user:4mW94qBdAWY5TnzZHKaY3cdA0cV8uEkD@dpg-d761n7ea2pns73fmf080-a/postgres3308")
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball Table Successfully Dropped'
+
+
+
+    
