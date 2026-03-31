42d6ae1518ed9f531216314476c25b00c1a4d187
Carl Seaholm <cdseaholm@gmail.com>
2026-03-31 19:14:20 +0000

    db_test changes

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from --Carl Seaholm-- in 3308'
 
-@app.route('db_test')
-def db_test():
+@app.route('/db_test')
+def testing():
     conn = psycopg2.connect("postgresql://postgres3308_user:4mW94qBdAWY5TnzZHKaY3cdA0cV8uEkD@dpg-d761n7ea2pns73fmf080-a/postgres3308")
     conn.close()
     return 'Database Connection Successsful'
